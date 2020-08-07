9f915537770f04a686a0c77c22c64d62624d922b
Nguyễn Minh Tiến <58099088+ngminhtien98@users.noreply.github.com>
2020-08-07 20:43:24 +0700

    Second Time

--- index.js
+++ index.js
@@ -6,35 +6,101 @@ app.set("view engine","ejs");
 app.listen(PORT, function () {
     console.log("Server is running...");
 })
+const fs = require("fs");
+
 
 app.get("/",function (req,res) {
+    let article = fs.readFileSync("data/data-article.json","UTF-8");
+    article = JSON.parse(article);
     let title = "Home";
+    article = orangize(article,3);
     res.render("home",
         {
             title: title,
+            article:article
+        });
+});
+
+app.get("/booking",function (req,res) {
+    let title = "Book An Appoinment";
+    let article = fs.readFileSync("data/data-article.json","UTF-8");
+    article = JSON.parse(article);
+    article = orangize(article,3);
+    res.render("pageBooking",
+        {
+            title: title,
+            article:article
+
+        });
+});
+
+
+app.get("/shopping",function (req,res) {
+    let title = "Shopping";
+    let article = fs.readFileSync("data/data-article.json","UTF-8");
+    article = JSON.parse(article);
+    article = orangize(article,3);
+    res.render("pageShop",
+        {
+            title: title,
+            article:article
         });
 });
 
 app.get("/blog",function (req,res) {
-    let title = "Home";
-    res.render("home",
+    let title = "Blog";
+    let article = fs.readFileSync("data/data-article.json","UTF-8");
+    article = JSON.parse(article);
+    article = orangize(article,3);
+    res.render("pageBlog",
         {
             title: title,
+            article:article
+
         });
 });
 
-app.get("/shop",function (req,res) {
-    let title = "Home";
-    res.render("home",
+app.get("/contact",function (req,res) {
+    let title = "Contact Us";
+    let article = fs.readFileSync("data/data-article.json","UTF-8");
+    article = JSON.parse(article);
+    article = orangize(article,3);
+    res.render("pageContact",
         {
             title: title,
+            article:article
         });
 });
 
 app.get("/blog/:id",function (req,res) {
-    let title = "Home";
-    res.render("home",
-        {
-            title: title,
-        });
+    let title = "Article";
+    let ID = req.params.id;
+
+    let article = fs.readFileSync("data/data-article.json","UTF-8");
+    article = JSON.parse(article);
+    articleNew = orangize(article,3);
+
+    let count = 0;
+    article.map(function (e) {
+        count++;
+        if(e.id == ID) {
+            res.render("pageArticle", {
+                cat: e,
+                article:articleNew
+            });
+            count = 0;
+        }
+    })
+    if(count >= article.length){
+        res.send("Not found")
+    }
+
 });
+function orangize(ary,num) {
+    ary = ary.reverse()
+    return ary.slice(0,num)
+}
+
+function tag(ary) {
+
+}
